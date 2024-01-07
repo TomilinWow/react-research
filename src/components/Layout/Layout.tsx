65d944ba476c07c4a8fcde0cd38c0f44150528fc
tomilin229 <tomilin229@gmail.com>
2024-01-07 21:31:18 +0300

    bug fix

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ export const Layout: FC<ILayout> = ({children}) => {
   return (
     <main className="flex h-screen flex-col items-center overflow-hidden bg-blue-200 from-slate-300 via-slate-50 to-slate-300">
       <nav className="flex h-10 w-full shrink-0 grow-0 items-center border-b border-slate-600 bg-blue-500">
-        <div className="flex w-full gap-4 p-2 font-semibold md:max-w-2xl">
+        <div className="flex w-full gap-4 p-2 font-semibold md:max-w-2xl text-white">
           <Link to="/">
             <span>Home</span>
           </Link>
